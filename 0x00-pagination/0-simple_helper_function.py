592b877e0f2f6183af888e3339c6428f4f51d0c6
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-04-02 02:35:47 +0100

    added task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''A simple helper function'''
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    return a tuple of size two containing a start
+    index and an end index of a page
+    '''
+
+    start_index = (page - 1) * page_size
+
+    end_index = start_index + page_size
+
+    return start_index, end_index
