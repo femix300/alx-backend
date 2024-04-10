04c778ff8e74cd904da7ee1cec8180ea6dffb0fe
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-04-10 11:47:40 +0100

    added task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Basic Babel setup'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''A config class'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    '''calls render template on the html file'''
+    return render_template('1-index.html')
+
+
+def get_locale():
+    '''
+    Determine the best match between supported languages
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app.run(debug=True)
+babel.init_app(app, locale_selector=get_locale)
