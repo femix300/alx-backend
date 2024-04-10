b1fca2ab34e57c1be1b269cc298315ba1a03386d
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-04-10 15:42:05 +0100

    added task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Basic Babel setup'''
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
 
@@ -18,13 +18,26 @@ class Config:
 app.config.from_object(Config)
 
 
+def get_user():
+    login_as = request.args.get('login_as')
+    if login_as:
+        user = users.get(int(login_as))
+        return user
+    return None
+
+
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     '''calls render template on the html file'''
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
-@babel.localeselector
+# @babel.localeselector
 def get_locale() -> str:
     '''
     Determine the best match between supported languages
@@ -34,6 +47,14 @@ def get_locale() -> str:
         if locale in app.config['LANGUAGES']:
             return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 if __name__ == '__main__':
